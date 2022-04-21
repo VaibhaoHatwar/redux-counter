97626786a3a434d155e02bdd9419764ec13e2edb
Vaibhao-H <vaibhaohatwar.works@gmail.com>
2022-04-21 20:00:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux"
+import { increment } from "./actions"
+import { decrement } from "./actions"
+import { reset } from "./actions"
 
-function App() {
+const App = () => {
+  const counter = useSelector((state) => state.counter)
+  const isLogged = useSelector((state) => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-6  text-center">
+          <h2>redux counter</h2>
+          <h1>{counter}</h1>
+          <div className="btn-group py-3">
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => dispatch(decrement())}
+            >
+              decrease
+            </button>
+            <button
+              className="btn btn-outline-success"
+              onClick={() => dispatch(reset())}
+            >
+              reset
+            </button>
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => dispatch(increment())}
+            >
+              increase
+            </button>
+          </div>
+          {isLogged ? <h3>Valuable information I shouldn't see</h3> : ""}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
